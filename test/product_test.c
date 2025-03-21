6575a601f834c545d56ad2db92b47b36a1b6ebff
AndreReyesG <jandre.reyesg@gmail.com>
2025-03-21 14:45:03 -0600

    style(product_test): comentarios

--- test/product_test.c
+++ test/product_test.c
@@ -6,6 +6,7 @@
 void setUp(void) {}
 void tearDown(void) {}
 
+// Retorna el tamaño (en bytes) de un archivo
 long fsize(FILE *f) {
   long actual = ftell(f);
   fseek(f, 0, SEEK_END);
@@ -15,25 +16,29 @@ long fsize(FILE *f) {
 }
 
 void test(void) {
+  // inicializamos la lista
   node_t *p = NULL;
+
+  // le agremos elementos
   product_t prod = new_product("strudels", "pan y pastelillos", 61945, 1, 183.13, 240.0, 20250322);
   add_product(&p, prod);
 
+  // crear un archivo temporal
   const char* filename = "test.txt";
   FILE *file;
 
-  // open and print the products in test.txt
+  // abrir el archivo temporal y escribir en el 
   file = fopen(filename, "w+b");
   show_products(file, p);
   fclose(file);
 
-  // open and read the file
+  // abrir el archivo temporal con permiso de escribir 
   file = fopen(filename, "r+b");
   char *buffer = (char*)malloc(sizeof(char) * fsize(file));
   char *aux = (char*)malloc(sizeof(char));
   fread(aux, sizeof(char), 1, file);
 
-  // TODO: forma más eficiente de leer el archivo
+  // TODO: Leer el contenido del archivo de forma más eficiente
   while ( !feof(file) ) {
     strcat(buffer, aux);
     fread(aux, sizeof(char), 1, file);
@@ -42,6 +47,7 @@ void test(void) {
   free(aux);
   fclose(file);
 
+  // eliminar el archivo temporal
   if ( remove(filename) == -1 ) {
     perror("Error deleting file");
   }
